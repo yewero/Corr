7ff6e119b4072cdcf6fbf8a33ed19d5295b1a2a2
Feng Tian <tianf@mail.cbi.pku.edu.cn>
2020-08-18 15:04:22 +0800

    support multiple CPUs in Corr

--- Corr.r
+++ Corr.r
@@ -1,32 +1,44 @@
 
-Corr <- function(expr_in) {
+library("parallel")
+
+Corr <- function(expr_in, ncpu = 1) {
   nCell <- nrow(expr_in)
   nGene <- ncol(expr_in)
-  corMat <- matrix(NA, nrow = nCell, ncol = nCell)
+  corMat <- matrix(0, nrow = nCell, ncol = nCell)
   rownames(corMat) <- rownames(expr_in)
   colnames(corMat) <- rownames(expr_in)
   
-  for(i in 1:nCell) {
-    for(j in 1:nCell) {
-      #cat(">", nCell, ":", i, j, "\n")
-      avgOther <- colMeans(expr_in[- c(i, j), ])
-      
-      u1 <- rep(0, nGene)
-      u1[expr_in[i, ] > avgOther] <- 1
-      u1[expr_in[i, ] < avgOther] <- -1
-      
-      u2 <- rep(0, nGene)
-      u2[expr_in[j, ] > avgOther] <- 1
-      u2[expr_in[j, ] < avgOther] <- -1
-      
-      corMat[i, j] <- cor(u1, u2, method = "pearson")
-    }
-  }
+  idx_DF <- data.table::CJ(1:nCell, 1:nCell)[, c(2, 1)]
+  colnames(idx_DF) <- c("C1", "C2")
+  idx_DF <- subset(idx_DF, C1 > C2)
+  
+  cl <- makeCluster(ncpu, type = "SOCK")
+  cor_value <- parApply(cl, idx_DF, 1, function(x) {
+    i <- x[1]
+    j <- x[2]
+    #cat(">", nCell, ":", i, j, "\n")
+    avgOther <- colMeans(expr_in[- c(i, j), ])
+    u1 <- rep(0, nGene)
+    u1[expr_in[i, ] > avgOther] <- 1
+    u1[expr_in[i, ] < avgOther] <- -1
+    
+    u2 <- rep(0, nGene)
+    u2[expr_in[j, ] > avgOther] <- 1
+    u2[expr_in[j, ] < avgOther] <- -1
+    
+    y <- cor(u1, u2, method = "pearson")
+    return(y)
+  })
+  stopCluster(cl); rm(cl)
+  
+  corMat[lower.tri(corMat)] <- cor_value
+  corMat <- corMat + t(corMat)
+  diag(corMat) <- 1
   return(corMat)
 }
 
 # set.seed(1)
-# expr_test <- matrix(runif(n = 20000), nrow = 10)
+# expr_test <- matrix(runif(n = 20000), nrow = 50)
 # rownames(expr_test) <- paste0("C", 1:nrow(expr_test))
 # 
-# cor_res <- Corr(expr_in = expr_test)
+# cor_res <- Corr(expr_in = expr_test, ncpu = 5)
